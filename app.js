ce775f7ec09f68d590ade2adbf93a15287c45984
keyclue <developer@keyclue.com>
2019-01-26 22:12:01 +0900

    MongoDB Env Variable changed

--- app.js
+++ app.js
@@ -1,41 +1,43 @@
-var dotenv = require('dotenv').config()
+var dotenv = require('dotenv').config();
 
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    passport    = require("passport"),
-    cookieParser = require("cookie-parser"),
-    LocalStrategy = require("passport-local"),
-    flash        = require("connect-flash"),
-    photo  = require("./models/photo"),
-    Comment     = require("./models/comment"),
-    User        = require("./models/user"),
-    session = require("express-session"),
-    seedDB      = require("./seeds"),
-    methodOverride = require("method-override");
-    
+var express = require('express'),
+	app = express(),
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose'),
+	passport = require('passport'),
+	cookieParser = require('cookie-parser'),
+	LocalStrategy = require('passport-local'),
+	flash = require('connect-flash'),
+	photo = require('./models/photo'),
+	Comment = require('./models/comment'),
+	User = require('./models/user'),
+	session = require('express-session'),
+	seedDB = require('./seeds'),
+	methodOverride = require('method-override');
+
 //requiring routes
-var commentRoutes    = require("./routes/comments"),
-    photoRoutes = require("./routes/photos"),
-    indexRoutes = require("./routes/index"),
-    tmallRoutes = require("./routes/tmall")
-    
-mongoose.connect("mongodb://localhost/keyclue_image", { useNewUrlParser: true });
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
+var commentRoutes = require('./routes/comments'),
+	photoRoutes = require('./routes/photos'),
+	indexRoutes = require('./routes/index'),
+	tmallRoutes = require('./routes/tmall');
+
+mongoose.connect(process.env.MONGO_DB_URI, { useNewUrlParser: true });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 app.use(methodOverride('_method'));
 app.use(cookieParser('secret'));
 
 // seedDB(); //seed the database
 
 // PASSPORT CONFIGURATION
-app.use(require("express-session")({
-    secret: "Once again Rusty wins cutest dog!",
-    resave: false,
-    saveUninitialized: false
-}));
+app.use(
+	require('express-session')({
+		secret: 'Once again Rusty wins cutest dog!',
+		resave: false,
+		saveUninitialized: false
+	})
+);
 
 app.use(flash());
 app.use(passport.initialize());
@@ -44,34 +46,34 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
-   res.locals.currentUser = req.user;
-   res.locals.success = req.flash('success');
-   res.locals.error = req.flash('error');
-   next();
+app.use(function(req, res, next) {
+	res.locals.currentUser = req.user;
+	res.locals.success = req.flash('success');
+	res.locals.error = req.flash('error');
+	next();
 });
 
 var cloudinary = require('cloudinary');
-cloudinary.config({ 
-  cloud_name: 'keyclue', 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET
+cloudinary.config({
+	cloud_name: 'keyclue',
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 app.locals.cloudinary = cloudinary;
 
-app.use("/", indexRoutes);
-app.use("/photos", photoRoutes);
-app.use("/photos/:id/comments", commentRoutes);
+app.use('/', indexRoutes);
+app.use('/photos', photoRoutes);
+app.use('/photos/:id/comments', commentRoutes);
 
-var apiRoutes = require("./routes/api");
-app.use("/api", apiRoutes);
-var tmallRoutes = require("./routes/tmall");
-app.use("/tmall", tmallRoutes);
+var apiRoutes = require('./routes/api');
+app.use('/api', apiRoutes);
+var tmallRoutes = require('./routes/tmall');
+app.use('/tmall', tmallRoutes);
 
-var dropzoneRoutes = require("./routes/dropzone");
-app.use("/dropzone", dropzoneRoutes);
+var dropzoneRoutes = require('./routes/dropzone');
+app.use('/dropzone', dropzoneRoutes);
 
-app.listen(3000 || process.env.PORT, process.env.IP, function(){
-   console.log("The Keyclue Server Has Started!");
+app.listen(3000 || process.env.PORT, process.env.IP, function() {
+	console.log('The Keyclue Server Has Started!');
 });
